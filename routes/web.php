95c612ec97004d7dfb1cbecb6df689136008bf8b
Maysara Safwat <maisarasafwat2@gmail.com>
2023-03-21 14:54:02 +0200

    added user authentication

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CommentsController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,15 +19,29 @@ use App\Http\Controllers\CommentsController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
-Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
-Route::get('/create', [PostsController::class, 'create'])->name('posts.create');
-Route::get('/update/{post}', [PostsController::class, 'updateForm'])->name('posts.updateForm');
-Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
-Route::put('/update/{post}', [PostsController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}', [PostsController::class, 'delete'])->name('posts.delete');
 
-Route::post('/comments/{post}', [CommentsController::class, 'store'])->name('comments.store');
-Route::get('/comments/{post}/editForm', [CommentsController::class,"edit"])->name("comments.edit");
-Route::put('/comments/{post}', [CommentsController::class,"update"])->name("comments.update");
-Route::delete('/comments/{post}', [CommentsController::class,"destroy"])->name("comments.delete");
+
+
+Route::group(['middleware'=>['auth']],function(){
+    Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
+    Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
+    Route::get('/create', [PostsController::class, 'create'])->name('posts.create');
+    Route::get('/update/{post}', [PostsController::class, 'updateForm'])->name('posts.updateForm');
+    Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+    Route::put('/update/{post}', [PostsController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostsController::class, 'delete'])->name('posts.delete');
+    
+    Route::post('/comments/{post}', [CommentsController::class, 'store'])->name('comments.store');
+    Route::get('/comments/{post}/editForm', [CommentsController::class,"edit"])->name("comments.edit");
+    Route::put('/comments/{post}', [CommentsController::class,"update"])->name("comments.update");
+    Route::delete('/comments/{post}', [CommentsController::class,"destroy"])->name("comments.delete");
+  
+  
+  });
+
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
