de27f94adffe88f2ad594dcdb2d6c7f9d4ca904e
Maysara Safwat <maisarasafwat2@gmail.com>
2023-03-26 19:37:41 +0200

    paginate with eager loading

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $allPosts = Post::all(); //select * from posts
+        $allPosts=Post::with('user')->paginate(5);
 
         return PostResource::collection($allPosts);
         
